c2100d7ffada46a78683c2fe5712469db995b50a
vimleshsinghk <vimleshsinghk@gmail.com>
2021-08-08 23:07:06 +0100

    Initial api boilerplate setup

---
+++ app.js
@@ -0,0 +1,56 @@
+const createError = require('http-errors');
+const express = require('express');
+const logger = require('morgan');
+
+const indexRouter = require('./routes/index');
+const app = express();
+
+const cors = require('cors'); //import cors module
+
+const whitelist = ['http://localhost:3000']; //white list consumers
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (whitelist.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(null, false);
+    }
+  },
+  methods: ['GET', 'PUT', 'POST', 'DELETE', 'OPTIONS'],
+  optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+  credentials: true, //Credentials are cookies, authorization headers or TLS client certificates.
+  allowedHeaders: [
+    'Content-Type',
+    'Authorization',
+    'X-Requested-With',
+    'device-remember-token',
+    'Access-Control-Allow-Origin',
+    'Origin',
+    'Accept'
+  ]
+};
+
+app.use(cors(corsOptions)); //adding cors middleware to the express with above configurations
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.use('/', indexRouter);
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function (err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
