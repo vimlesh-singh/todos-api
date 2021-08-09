13375257f4ec946e936f3634d633c2915d96165f
vimleshsinghk <vimleshsinghk@gmail.com>
2021-08-09 01:19:53 +0100

    removed dummy todos list from api call and also changed " to '

--- routes/todos.js
+++ routes/todos.js
@@ -1,8 +1,9 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
-const todos = require("../dummy-data/todos");
+// const todos = require("../dummy-data/todos");
+const todos = [];
 
-router.get("/", async function (req, res, next) {
+router.get('/', async function (req, res, next) {
   try {
     res.status(200).send(todos);
   } catch (error) {
@@ -11,12 +12,12 @@ router.get("/", async function (req, res, next) {
   }
 });
 
-router.post("/", async function (req, res, next) {
+router.post('/', async function (req, res, next) {
   try {
     const task =
-      req.body.task || req.body.task.trim() ? req.body.task.trim() : "";
+      req.body.task || req.body.task.trim() ? req.body.task.trim() : '';
     if (!task) return res.status(403).send(`task can't be empty`);
-    if (task === "") return res.status(403).send(`task can't be empty`);
+    if (task === '') return res.status(403).send(`task can't be empty`);
     if (todos.filter(t => t.task === task).length > 0)
       return res.status(403).send(`task already exist`);
     const todo = { id: todos.length + 1, task, isCompleted: false };
@@ -28,14 +29,14 @@ router.post("/", async function (req, res, next) {
   }
 });
 
-router.put("/", async function (req, res, next) {
+router.put('/', async function (req, res, next) {
   try {
     const id =
       req.body.id || req.body.id.toString().trim()
         ? req.body.id.toString().trim()
-        : "";
+        : '';
     if (!id) return res.status(403).send(`Error updating task`);
-    if (id === "") return res.status(403).send(`Error updating task`);
+    if (id === '') return res.status(403).send(`Error updating task`);
     const found = todos.findIndex(t => t.id === Number(id));
     if (found < 0) return res.status(403).send(`Error updating task`);
     todos[found].isCompleted = !todos[found].isCompleted;
