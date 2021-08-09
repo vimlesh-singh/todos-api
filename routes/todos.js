8803c034361a8f3118583f89b4f4963e233c0578
vimleshsinghk <vimleshsinghk@gmail.com>
2021-08-09 01:02:03 +0100

    Task2: added api to add new todos in the list

--- routes/todos.js
+++ routes/todos.js
@@ -11,4 +11,21 @@ router.get("/", async function (req, res, next) {
   }
 });
 
+router.post("/", async function (req, res, next) {
+  try {
+    const task =
+      req.body.task || req.body.task.trim() ? req.body.task.trim() : "";
+    if (!task) return res.status(403).send(`task can't be empty`);
+    if (task === "") return res.status(403).send(`task can't be empty`);
+    if (todos.filter(t => t.task === task).length > 0)
+      return res.status(403).send(`task already exist`);
+    const todo = { id: todos.length + 1, task, isCompleted: false };
+    todos.unshift(todo);
+    res.status(201).send(todos);
+  } catch (error) {
+    console.log(req.body, error);
+    res.status(500).send(`bad request`);
+  }
+});
+
 module.exports = router;
